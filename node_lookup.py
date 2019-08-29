0918fe26b78f27c602cb9015bd9a1bb98473a5d7
Hossam Amer <hossam.amer@multicom-mac2.uwaterloo.ca>
2019-08-29 15:36:23 -0400

    Add node look up

---
+++ node_lookup.py
@@ -0,0 +1,58 @@
+
+
+
+import os
+import tensorflow as tf
+MODEL_PATH = '/Users/hossam.amer/7aS7aS_Works/work/jpeg_ml_research/inceptionv3/inception_model'
+
+# Change class ID to human readable label
+class NodeLookup(object):
+    def __init__(self):  
+        label_lookup_path = os.path.join(MODEL_PATH, 'imagenet_2012_challenge_label_map_proto.pbtxt')
+        uid_lookup_path = os.path.join(MODEL_PATH, 'imagenet_synset_to_human_label_map.txt')
+        self.node_lookup = self.load(label_lookup_path, uid_lookup_path)
+
+    def load(self, label_lookup_path, uid_lookup_path):
+        # 加载分类字符串n********对应分类名称的文件
+        proto_as_ascii_lines = tf.gfile.GFile(uid_lookup_path).readlines()
+        uid_to_human = {}
+        #一行一行读取数据
+        for line in proto_as_ascii_lines :
+            #去掉换行符
+            line=line.strip('\n')
+            #按照'\t'分割
+            parsed_items = line.split('\t')
+            #获取分类编号
+            uid = parsed_items[0]
+            #获取分类名称
+            human_string = parsed_items[1]
+            #保存编号字符串n********与分类名称映射关系
+            uid_to_human[uid] = human_string
+
+        # 加载分类字符串n********对应分类编号1-1000的文件
+        proto_as_ascii = tf.gfile.GFile(label_lookup_path).readlines()
+        node_id_to_uid = {}
+        for line in proto_as_ascii:
+            if line.startswith('  target_class:'):
+                #获取分类编号1-1000
+                target_class = int(line.split(': ')[1])
+            if line.startswith('  target_class_string:'):
+                #获取编号字符串n********
+                target_class_string = line.split(': ')[1]
+                #保存分类编号1-1000与编号字符串n********映射关系
+                node_id_to_uid[target_class] = target_class_string[1:-2]
+
+        #建立分类编号1-1000对应分类名称的映射关系
+        node_id_to_name = {}
+        for key, val in node_id_to_uid.items():
+            #获取分类名称
+            name = uid_to_human[val]
+            #建立分类编号1-1000到分类名称的映射关系
+            node_id_to_name[key] = name
+        return node_id_to_name
+
+    #传入分类编号1-1000返回分类名称
+    def id_to_string(self, node_id):
+        if node_id not in self.node_lookup:
+            return ''
+        return self.node_lookup[node_id]
